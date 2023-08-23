53ec13dd550cb1e56153db2bf9947c0d8eca0c4c
Bieldeid <gabrieldeid.android@gmail.com>
2023-08-23 11:34:42 -0300

    Aula03 realizada. ✔

---
+++ BarrasColoridadasTV/app/src/main/java/fatec/mobile/aula03/MainActivity.java
@@ -0,0 +1,14 @@
+package fatec.mobile.aula03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.teste_frame_layout);
+    }
+}
