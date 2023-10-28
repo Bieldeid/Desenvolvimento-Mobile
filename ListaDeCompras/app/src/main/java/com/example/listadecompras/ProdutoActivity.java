e475dcf2b59bb7978d5fd30de77dc79e60d86d42
Bieldeid <gabrieldeid.android@gmail.com>
2023-10-28 01:14:23 -0300

    Lista de Produtos finalizada ✅

--- ListaDeCompras/app/src/main/java/com/example/listadecompras/ProdutoActivity.java
+++ ListaDeCompras/app/src/main/java/com/example/listadecompras/ProdutoActivity.java
@@ -12,6 +12,9 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -55,12 +58,14 @@ public class ProdutoActivity extends AppCompatActivity {
         btnGravar = findViewById(R.id.btnGravar);
         btnPesquisar = findViewById(R.id.btnPesquisar);
 
+        btnPesquisar.setOnClickListener( e -> pesquisar() );
+
         btnGravar.setOnClickListener( e -> gravar() );
         carregarFirebase();
     }
 
-    public void carregarFirebase( Produto l ) {
-        String jsonLista = gson.toJson(l);
+    public void salvarFirebase(Produto p) {
+        String jsonLista = gson.toJson(p);
 
         ExecutorService executor = Executors.newSingleThreadExecutor();
         Handler handler = new Handler(Looper.getMainLooper());
@@ -84,12 +89,85 @@ public class ProdutoActivity extends AppCompatActivity {
         });
     }
 
+    private Produto paraEntidade(){
+        Produto p = new Produto();
+        p.setNomeProduto(txtNome.getText().toString());
+        p.setTipoProduto(txtTipo.getText().toString());
+        p.setQuantidade(Float.parseFloat(txtQuantidade
+                .getText().toString()));
+        p.setPrecoUnitario(Double.parseDouble(txtPreco
+                .getText().toString()));
+        return p;
+    }
+
     private void gravar(){
-
+        Produto p = paraEntidade();
+        produtos.add(p);
+        salvarFirebase(p);
+        mostrarLista();
     }
 
-    private void carregarFirebase(){
-        
+    private void pesquisar(){
+        String nome = txtNome.getText().toString();
+        Log.d(APP_LISTA, "Pesquisando restaurante: + nome + );
+        for (Produto p : produtos) {
+            Log.d(APP_LISTA, "Restaurante: (" + p.getNomeProduto() + ") contém " +
+                    p.getNomeProduto().contains( nome ) );
+            if (p.getNomeProduto().contains(nome)){
+                txtNome.setText(p.getNomeProduto());
+                txtTipo.setText(p.getTipoProduto());
+                txtQuantidade.setText(String.valueOf(p.getQuantidade()) );
+                txtPreco.setText(String.valueOf(p.getPrecoUnitario()) );
+            }
+        }
     }
+
+    private void mostrarLista(){
+        for (Produto p: produtos){
+            Log.d(APP_LISTA, p.toString());
+        }
+    }
+
+    private void carregarFirebase() {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(() -> {
+            produtos.clear();
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(URL)
+                    .get()
+                    .build();
+            try (Response response = client.newCall(request).execute()) {
+                String resposta = response.body().string();
+
+                if (resposta != null && !resposta.isEmpty()) {
+                    JsonParser parser = new JsonParser();
+                    JsonElement element = parser.parse(resposta);
+
+                    if (element != null && !element.isJsonNull() && element.isJsonObject()) {
+                        JsonObject convertedObject = element.getAsJsonObject();
+
+                        for (String chave : convertedObject.keySet()) {
+                            JsonElement obj = convertedObject.get(chave);
+
+                            if (obj != null && !obj.isJsonNull() && obj.isJsonObject()) {
+                                Produto r = gson.fromJson(obj, Produto.class);
+                                produtos.add(r);
+                            }
+                        }
+                        Log.e(APP_LISTA, "Resposta: " + resposta);
+                    } else {
+                        Log.e(APP_LISTA, "Resposta do servidor não é um objeto JSON válido.");
+                    }
+                } else {
+                    Log.e(APP_LISTA, "Resposta do servidor vazia ou nula.");
+                }
+            } catch (IOException e) {
+                Log.e(APP_LISTA, "Erro: ", e);
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
 
 }
